7533306d8fe010e2e666806aefc076af178e00e4
yadavrajesh003 <yadav.rajesh003@gmail.com>
2020-09-18 11:40:55 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
+  templateUrl:'/app.component.html' //1
+ /* template: `
     <div><h1>{{pageTitle}}</h1>
       <div>My First Component</div>
     </div>
-    `
+    `*/
 })
 export class AppComponent {
   pageTitle: string = 'Product Management';
